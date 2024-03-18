2b343ad74d99064a87845214078a666f52378fbe
77Oyasumi <xihan1205@gmail.com>
2024-03-18 19:09:01 +0800

    checkbox select

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ import time
 driver = webdriver.Chrome()
 driver.get("https://gis.ris.gov.tw/dashboard.html?key=E04")
 
+county_values = [
+    "65000000", "63000000", "68000000", "66000000", "67000000",
+    "64000000", "10002000", "10004000", "10005000", "10007000",
+    "10008000", "10009000", "10010000", "10013000", "10014000",
+    "10015000", "10016000", "10017000", "10018000", "10020000",
+    "09020000", "09007000"
+]
+
 # 等待下拉選單元件出現
 select_year_element = WebDriverWait(driver, 5).until(
     EC.presence_of_element_located((By.CSS_SELECTOR, 'select.input-sm.mb-md:nth-of-type(1)'))
@@ -19,12 +27,35 @@ select_month_element = WebDriverWait(driver, 5).until(
 )
 select_month = Select(select_month_element)
 
-# 依次選擇97到113的年份
+select_county_element = WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.XPATH, "//button[@class='multiselect dropdown-toggle btn btn-default']"))
+)
+
+checkbox_elements = WebDriverWait(driver, 5).until(
+    EC.presence_of_all_elements_located((By.XPATH, '//label[@class="checkbox"]/input[@type="checkbox"]'))
+)
+
+#依次選擇97到113的年份
 for year in range(97, 114):
     select_year.select_by_value(str(year))
     time.sleep(1)
     select_month.select_by_value('12')
     time.sleep(1)
+    select_county_element.click()
+    time.sleep(1)
+    checkbox_elements = WebDriverWait(driver, 5).until(
+        EC.presence_of_all_elements_located((By.XPATH, '//label[@class="checkbox"]/input[@type="checkbox"]'))
+    )
+    for checkbox in checkbox_elements:
+        if checkbox.get_attribute('value') == '0':
+            checkbox.click()
+            time.sleep(1)
+    for value in county_values:
+        for checkbox in checkbox_elements:
+            if checkbox.get_attribute('value') == value:
+                checkbox.click()
+                time.sleep(1)
+                checkbox.click()
 
 # 停留幾秒後關閉瀏覽器
 time.sleep(5)
