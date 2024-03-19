5d2e5d7eee1de79712cc2d2920fb376893d41139
77Oyasumi <xihan1205@gmail.com>
2024-03-19 18:09:34 +0800

    finish with name tag

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import os
 
 driver = webdriver.Chrome()
 url = "https://gis.ris.gov.tw/dashboard.html?key=E04"
@@ -20,15 +21,14 @@ WebDriverWait(driver, 5).until(
 # submit_button = driver.find_element(By.XPATH,'//*[@id="searchPanel"]/div/div[1]/div[1]/div/div/select[1]').click()
 year_selection = Select(driver.find_element(By.XPATH,'//*[@id="searchPanel"]/div/div[1]/div[1]/div/div/select[1]'))
 month_selection = Select(driver.find_element(By.XPATH,'//*[@id="searchPanel"]/div/div[1]/div[1]/div/div/select[2]'))
+
 for year in range(97, 114):
     year_selection.select_by_value(str(year))
     month_selection.select_by_value(str(12))
     time.sleep(0.1)
 
-    # year_value = year_selection.first_selected_option.text
-    # print(f"year:{year_value}")
-    # month_value = month_selection.first_selected_option.text
-    # print(f"year:{month_value}")
+    year_value = year_selection.first_selected_option.text
+    month_value = month_selection.first_selected_option.text
     
     time.sleep(1)
     for i in range(2,30):
@@ -38,10 +38,16 @@ for year in range(97, 114):
             driver.find_element(By.XPATH,'//*[@id="searchPanel"]/div/div[1]/div[2]/div/div/div/ul/li[' + str(i) + ']/a/label/input').click()
             time.sleep(0.1)
             driver.find_element(By.XPATH,'//*[@id="searchPanel"]/div/div[3]/div/button').click()
+            area_name = driver.find_element(By.XPATH,'//*[@id="searchPanel"]/div/div[1]/div[2]/div/div/div/button').text
             time.sleep(1.5)
             driver.find_element(By.XPATH, '//*[@id="app"]/div/section/div/div[3]/section/header/div[1]/a[1]/i').click()
-            time.sleep(0.5)
+            time.sleep(1.5)
 
+            # 將重新命名的部分放在這裡
+            os.chdir(r'C:\Users\Oyasumi\Downloads')
+            org_name = r'全國人口資料庫統計地圖.csv'
+            new_name = f'{year}_{area_name}.csv'
+            os.rename(org_name, new_name)
 
         else:
             try:
@@ -51,12 +57,15 @@ for year in range(97, 114):
                 driver.find_element(By.XPATH,'//*[@id="searchPanel"]/div/div[1]/div[2]/div/div/div/ul/li[' + str(i) + ']/a/label/input').click()
                 time.sleep(0.1)
                 driver.find_element(By.XPATH,'//*[@id="searchPanel"]/div/div[3]/div/button').click()
+                area_name = driver.find_element(By.XPATH,'//*[@id="searchPanel"]/div/div[1]/div[2]/div/div/div/button').text
                 time.sleep(1.5)
                 driver.find_element(By.XPATH, '//*[@id="app"]/div/section/div/div[3]/section/header/div[1]/a[1]/i').click()
-                time.sleep(0.5)
+                time.sleep(1.5)
+
+               # 將重新命名的部分放在這裡
+                os.chdir(r'C:\Users\Oyasumi\Downloads')
+                org_name = r'全國人口資料庫統計地圖.csv'
+                new_name = f'{year}_{area_name}.csv'
+                os.rename(org_name, new_name)
             except:
                 continue
-
-        # path = "C:\Users\Oyasumi\Downloads"
-        # file_name = "全國人口資料庫統計地圖.csv"
-        # new_file  = "year/month/XXXX.csv"
